f6656ae9cf6ab343a5f479ae0f26977e13fc7005
Jack Footner <jackrfootner@gmail.com>
2020-10-12 16:07:45 +1030

    Create Install Script  - Fix Default Config for install

--- example_config.js
+++ example_config.js
@@ -1,15 +1,15 @@
-// The token the bot will use to login
-botToken: `1234567890poiuytrewqqwertyuiop0987654321`;
-// The text that will appear in the bot's status (e.g., 'Bot is playing ...')
-statusText: `@Radiobot Help`;
-// An array of user IDs which have full control over the bot
-ownerID: [`1234567890`, `0987654321`];
-// The level of information to display in the console (a level of 10 will display everything while a level of 40 will only show errors and base bot info)
-// 50 Base bot info (starting up etc)
-// 40 Errors
-// 30 Warnings
-// 20 Info
-// 10 Commands
-debugLevel: 50;
-
-export { botToken, statusText, ownerID, debugLevel }
+module.exports = {
+	// The token the bot will use to login
+	botToken: `1234567890poiuytrewqqwertyuiop0987654321`,
+	// The text that will appear in the bot's status (e.g., 'Bot is playing ...')
+	statusText: `@Radiobot Help`,
+	// An array of user IDs which have full control over the bot
+	ownerID: [`1234567890`, `0987654321`],
+	// The level of information to display in the console (a level of 10 will display everything while a level of 40 will only show errors and base bot info)
+	// 50 Base bot info (starting up etc)
+	// 40 Errors
+	// 30 Warnings
+	// 20 Info
+	// 10 Commands
+	debugLevel: 50,
+}
