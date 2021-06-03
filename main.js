5d91170170045fa99390d5e396f41fba888d4e42
Jack Footner <jackrfootner@gmail.com>
2021-06-03 13:08:46 +0930

    Run when function does exist

--- main.js
+++ main.js
@@ -140,11 +140,12 @@ function onReady() {
 
 	// Interval Functions
 	bot.setInterval(() => {
+		console.log('interval time');
 		// For each module
 		for (const moduleName in modules) {
 			if (!Object.hasOwnProperty.call(modules, moduleName)) continue;
 			const module = modules[moduleName];
-			if (typeof module.runOnInterval !== 'function') {
+			if (typeof module.runOnInterval === 'function') {
 				module.runOnInterval({
 					bot: bot,
 					store: store[moduleName],
